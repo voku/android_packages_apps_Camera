38bc5492cd9dcb8cb2889dfc8bba1e607f17689f
Jeykumar Sankaran <jsanka@codeaurora.org>
2011-06-23 09:10:20 +0800

    Delete icon texture's after draw
    This change calls delete on the icon texture once they are drawn
    
    Change-Id: I0b0107a194b6137a28ed300eabf5d6698fb7ea51
    CRs-Fixed: 290869

--- src/com/android/camera/ui/AbstractIndicator.java
+++ src/com/android/camera/ui/AbstractIndicator.java
@@ -62,6 +62,7 @@ public abstract class AbstractIndicator extends GLView {
                         p.left + (width - icon.getWidth()) / 2,
                         p.top + (height - icon.getHeight()) / 2);
             }
+            icon.deleteFromGL();
         }
     }
 
